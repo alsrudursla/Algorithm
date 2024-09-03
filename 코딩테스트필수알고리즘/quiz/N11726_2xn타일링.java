1e2842995b7514d3f6503defa0146ea6389ca62e
alsrudursla <nuy0307@naver.com>
2024-09-03 20:55:52 +0900

    DP

---
+++ 코딩테스트필수알고리즘/quiz/N11726_2xn타일링.java
@@ -0,0 +1,30 @@
+package 코딩테스트필수알고리즘.quiz;
+
+import java.io.*;
+
+// DP
+public class N11726_2xn타일링 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int N = Integer.parseInt(br.readLine());
+
+        // 1. N=1 : 2x1 1개
+        // 2. N=2 : 2x1 2개 or 1x2 2개 → 2가지
+        // 3. N=3 : 2x1 3개 or (2x1 1개 & N=2일때경우) → 3가지
+        // 점화식 An = An-1 + An-2
+
+        int[] square = new int[1001];
+        square[1] = 1;
+        square[2] = 2;
+
+        for (int i = 3; i <= N; i++) {
+            square[i] = (square[i-1] + square[i-2]) % 10007;
+        }
+
+        bw.write(String.valueOf(square[N]));
+        bw.newLine();
+        bw.flush();
+        bw.close();
+    }
+}
