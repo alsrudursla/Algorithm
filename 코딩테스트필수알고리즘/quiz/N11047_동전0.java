62c9b8adc48e9edfc3bb2bb6f3325f58ca7a7571
alsrudursla <nuy0307@naver.com>
2024-09-03 17:43:04 +0900

    그리디

---
+++ 코딩테스트필수알고리즘/quiz/N11047_동전0.java
@@ -0,0 +1,36 @@
+package 코딩테스트필수알고리즘.quiz;
+
+import java.io.*;
+import java.util.*;
+
+// 그리디
+public class N11047_동전0 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken()); // 동전 종류 개수
+        int K = Integer.parseInt(st.nextToken()); // 총 합
+
+        int[] coins = new int[N];
+        for (int i = 0; i < N; i++) {
+            coins[i] = Integer.parseInt(br.readLine());
+        }
+
+        int ans = 0;
+        for (int i = N-1; i >= 0; i--) {
+            if (coins[i] > K) {
+                continue;
+            }
+            int num = K / coins[i];
+            ans += num;
+
+            K = K - coins[i] * num;
+        }
+
+        bw.write(String.valueOf(ans));
+        bw.newLine();
+        bw.flush();
+        bw.close();
+    }
+}
